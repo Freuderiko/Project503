0b576e76772825a9a2c95ee9dc727dafbe215a1c
Guilherme Azevedo <guilherme@pop-os.localdomain>
2020-05-30 22:29:13 -0300

    Implementação do login de usuário.

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 
 module.exports = {
+  //Criação de usuário.
   async store(req, res) {
     const {email, password} = req.body
 
@@ -9,6 +10,19 @@ module.exports = {
 
     if(!user) {
       user = await User.create({email, password})
+    }
+
+    return res.json(user)
+  },
+
+  //Login.
+  async login(req, res) {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email, password})
+
+    if(!user) {
+      return res.send(401)
     }
 
     return res.json(user)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ const rouets = express.Router()
 //controllers.
 const UserController = require('./controllers/UserController')
 
+//User routes.
 rouets.post('/users', UserController.store)
+rouets.post('/login', UserController.login)
 
 module.exports = rouets
